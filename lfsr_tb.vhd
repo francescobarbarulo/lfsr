f86bbdb0906397fe7cfa3ab44279199f5a88f877
francescobarbarulo <barbarulofrancesco@gmail.com>
2019-01-02 12:13:35 +0100

    bug fixed

--- lfsr_tb.vhd
+++ lfsr_tb.vhd
@@ -36,14 +36,14 @@ architecture testbench of lfsr_tb is
     --------------------------------------------------------------
     -- clk signal initilized to '0'
     signal clk_tb : std_logic := '0';
-    -- rst_n signal initialized to '1'
-    signal rst_n_tb : std_logic := '1';
+    -- rst_n signal initialized to '0'
+    signal rst_n_tb : std_logic := '0';
     -- signal to stop the simulation
     signal stop_simulation : std_logic := '1';
     -- input of the lfsr
-    signal lfsr_i_tb : std_logic_vector(N_bit - 1 downto 0);
+    signal lfsr_i_tb : std_logic_vector(N - 1 downto 0);
     -- output of the lfsr
-    signal lfsr_o_tb : std_logic_vector(N_bit - 1 downto 0);
+    signal lfsr_o_tb : std_logic_vector(N - 1 downto 0);
     -- enable signal
     signal en_tb : std_logic := '0';
     
@@ -58,7 +58,7 @@ architecture testbench of lfsr_tb is
             port map(
                 lfsr_i => lfsr_i_tb,
                 lfsr_o => lfsr_o_tb,
-		        en => en_tb,
+		en => en_tb,
                 clk => clk_tb,
                 rst_n => rst_n_tb
             );
@@ -70,8 +70,8 @@ architecture testbench of lfsr_tb is
                 if(rising_edge(clk_tb)) then
                     case t is
                         when 0 => lfsr_i_tb <= "1010110011100001"; -- 0xACE1
-			            when 1 => rst_n_tb <= '1';
-			            when 3 => en_tb <= '1';
+				  rst_n_tb <= '1';
+			when 1 => en_tb <= '1';
                         when others => null;
                         
                     end case;
